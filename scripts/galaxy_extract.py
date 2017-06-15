8dde5cbf48e0fdc8898cde78ad967d55355114bb
Vince-ec <vestrada78840@gmail.com>
2017-06-15 15:35:09 -0500

    june 13

---
+++ scripts/galaxy_extract.py
@@ -0,0 +1,82 @@
+import grizli
+import numpy as np
+from vtl.Readfile import Readfile
+from astropy.io import fits
+from astropy.table import Table
+from astropy import wcs
+import pysynphot as S
+from glob import glob
+
+
+
+def Source_present(fn, ra, dec):
+    flt = fits.open(fn)
+    w = wcs.WCS(flt[1].header)
+    hdr = w.to_header(relax=True)
+    present = False
+
+    xpixlim = len(flt[1].data[0])
+    ypixlim = len(flt[1].data)
+
+    [pos] = w.wcs_world2pix([[ra, dec]], 1)
+
+    print pos
+
+    if -100 < pos[0] < xpixlim + 100 and -100 < pos[1] < ypixlim + 100 and flt[0].header['OBSTYPE'] == 'SPECTRA':
+        present = True
+
+    return present, pos
+
+def FLT_search(ra, dec):
+    fn=glob('../../../Clear_data/flt_files/i*flt.fits')
+
+    for i in range(len(fn)):
+        pres,pos=Source_present(fn[i],ra,dec)
+        if pres==True:
+            print fn[i],pos
+
+
+
+class Image_pull(object):
+    def __init__(self, flt_input, mosaic, segment_map, reference_catalog, galaxy_id, pad=100):
+        self.flt_input = flt_input
+        self.mosaic = mosaic
+        self.segment_map = segment_map
+        self.reference_catalog = reference_catalog
+        self.galaxy_id = galaxy_id
+        self.pad = pad
+
+        """ 
+        self.flt_input - grism flt (not image flt) which contains the object you're interested in modeling, this
+                         will tell Grizli the PA
+        **                 
+        self.mosaic - mosaic image 
+        **
+        self.segment_map - segmentation map for the corresponding mosaic
+        **
+        self.reference_catalog - reference catalog which matches the segmentation map, currently this must be an 
+                                 ascii file, this can be changed later to include fits files
+        **
+        self.galaxy_id - ID of the object you want to model
+        **
+        self.pad - Grizli uses this to add extra pixels to the edge of an image to account for galaxies near the 
+                   edge, 100 is usually enough
+        **
+        self.cutout - cutout of galaxy
+        """
+
+        ## Create Grizli model object
+        sim_g102 = grizli.model.GrismFLT(grism_file=self.flt_input, verbose=False,
+                                         ref_file=self.mosaic, seg_file=self.segment_map,
+                                         force_grism='G102', pad=self.pad)
+
+        ref_cat = Table.read(self.reference_catalog, format='ascii')
+
+        sim_cat = sim_g102.blot_catalog(ref_cat, sextractor=False)
+
+        sim_g102.compute_full_model(ids=sim_cat['id'], mags=Mag(sim_cat['f_F125W']))
+
+        ## Get Cutout
+        beam_g102 = grizli.model.BeamCutout(sim_g102, sim_g102.object_dispersers[self.galaxy_id]['A'])
+
+        self.cutout = beam_g102
--- scripts/test.py
+++ scripts/test.py
@@ -1,27 +1,15 @@
+import galaxy_extract as ge
 import numpy as np
-from spec_id import Galaxy_set
 import pandas as pd
 import matplotlib.pyplot as plt
-from vtl.Readfile import Readfile
-from glob import glob
-import shutil
-from astropy.io import fits, ascii
-from astropy.table import Table
-from scipy.interpolate import interp1d
-import os
-import cPickle
 import seaborn as sea
+from glob import glob
 sea.set(style='white')
 sea.set(style='ticks')
 sea.set_style({"xtick.direction": "in","ytick.direction": "in"})
 colmap = sea.cubehelix_palette(12, start=2, rot=.2, dark=0, light=1.1, as_cmap=True)
 
-qgalDB = pd.read_pickle('../data/quiescent_gal_DB.pkl')
-all_g_gals = list(qgalDB[qgalDB['in_data'] == True][qgalDB['agn'] == False]['gids'])
+qgDB = pd.read_pickle('../data/quiescent_gal_DB.pkl')
+gsDB = qgDB[qgDB['spec'] == True]
 
-for i in range(len(all_g_gals)):
-    galaxy = all_g_gals[i]
-    n_dir = '../../../../../Volumes/Vince_homedrive/Extractions/Quiescent_galaxies/%s' % galaxy
-    spec = glob(n_dir + '/*.npy')
-    if len(spec) > 0:
-        shutil.copyfile(spec[0],'/Users/Vince.ec/Github/Quiescent_analysis/spec_stacks_june14/%s_stack.npy' % galaxy)
+ge.FLT_search(gsDB['ra'][220],gsDB['dec'][220])
